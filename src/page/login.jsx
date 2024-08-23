7117f716e79497e3069274e835a6453d72913535
unknown <seesnight@naver.com>
2024-08-23 19:48:41 +0900

    Feat: 로그인페이지 수정

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -4,34 +4,30 @@ import React, { useState } from "react";
 export default function WaitingRoom() {
 
   return (
-    <div className="container">
-      <div className="content-area"></div>
-      <div className="title">
-        <p>배틀글라운드</p>
+    <div className="lg-container">
+      <div className="lg-content-area"></div>
+      <div className="lg-title">
+        <img className="img1" src="/image/irumae_happy.png"/>
+        <h1>배틀글라운드</h1>
+        <img className='img2' src="/image/irumae_sad.png"/>
       </div>
-      <div className="leftcontainer">
-        <p>
-          이름:
-          <input></input>
-        </p>
-        <p>
-          전화번호:
-          <input></input>
-        </p>
+      <div className="lg-leftcontainer">
+        <h2>이름</h2>
+        <input/>
+        <h2>전화번호</h2>
+        <input />
         <button>로그인</button>
       </div>
-      <div className="rightcontainer">
-        <div className="background-image"></div>
-        <div className="info-section">
+      <div className="lg-rightcontainer">
+        <div className="lg-background-image"></div>
+        <div className="lg-info-section">
         <h1>(중요) 게임 정보 (중요)</h1>
-        <h2>✔ 이예나배 천하제일 타자 대회입니다.</h2>
-        <h2>✔ 팀원과 합심하여 모든 단어를 없애보세요.</h2>
-        <h2>✔ 게임 시간은 60초 입니다.</h2>
-        <h2>✔ 각 단어 점수 체계는 어쩌구 저쩌구 이렇게 됩니다.</h2>
-        <h2>✔ 남은 시간이 10초 이하가 되면 경고 표시가 나옵니다.</h2>
-        <h2>✔ 1등에게는 원용걸 친필 사인을</h2>
-        <h2>✔ 2등에게는 전액 장학금을 드립니다.</h2>
-        </div>
+        <h2>✔ 타자 고수들의 눈치싸움!</h2>
+        <h2>✔ 게임 시간은 <strong>60초</strong> 입니다. 남은 시간이 <strong>10초</strong> 이하가 되면 경고 표시가 나옵니다.</h2>
+        <h2>✔ 세 명의 플레이어가 1분 동안 화면에 등장하는 단어들을 빠르게 타이핑하며 경쟁합니다. 단어를 성공적으로 입력하면 <strong>10점</strong>을 획득하고, 그 단어는 다른 플레이어의 화면에서도 사라집니다. 누가 먼저 입력하느냐에 따라 점수가 달라지니, 스피드와 타이밍이 모두 중요합니다.</h2>
+        <h2>✔ 1분 동안 최대한 많은 단어를 입력해 <strong>Top 10</strong>에 도전하세요!</h2>
+        <h2>✔ 눈치와 타이핑 실력으로 승부를 가리는 긴장감 넘치는 타자게임, 지금 시작해보세요!</h2>
+          </div>
       </div>
     </div>
   );
